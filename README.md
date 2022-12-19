ca2217035f7b1f9577d7d38ddc10c280734404a1
tomaszkubik98 <tomasz.kubik98@outlook.com>
2022-12-19 19:17:36 +0100

    Update

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pomodorro
+
+## Table of contents
+
+* [About](#about)
+* [Screenshots](#screenshots)
+* [How to use](#how-to-use)
+
+
+## About
+
+**Pomodorro** is one of my projects realized in [Python Bootcamp](https://www.udemy.com/course/100-days-of-code/)
+Use :tomato: and optimise your work schedule (25 min work -> 5 min break)
+
+## Screenshots
+
+* Work
+
+![Screen](screenshots/work-window.png)
+
+* Break
+
+![Screen](screenshots/break-window.png)
+
+## How to use
+
+Simply run the script and press start button to initialize count down timer, after 25 minutes pass take a shot 5 min breake.
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps=0
@@ -74,7 +74,7 @@ window.config(padx=100,pady=50,bg=YELLOW)
 title_txt = Label(text="Timer",font=(FONT_NAME,50,"bold"),fg=GREEN,bg=YELLOW)
 title_txt.grid(column=2,row=1)
 
-tomato_img = PhotoImage(file="tomato.png")
+tomato_img = PhotoImage(file="static/tomato.png")
 canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 canvas.create_image(100,112, image=tomato_img)
 timer_txt = canvas.create_text(100,130,text="00:00",fill="white", font=(FONT_NAME,35,"bold"))
